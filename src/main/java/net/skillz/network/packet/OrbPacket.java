e7bd5dae1dd3817d8da79fc4e3af30d222713597
Sygikal <xtin8873@gmail.com>
2025-05-04 17:12:59 -0700

    idk

--- src/main/java/net/skillz/network/packet/OrbPacket.java
+++ src/main/java/net/skillz/network/packet/OrbPacket.java
@@ -1,5 +1,8 @@
 package net.skillz.network.packet;
 
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.util.Identifier;
+import net.skillz.SkillZMain;
 import net.skillz.access.OrbAccess;
 import net.skillz.entity.LevelExperienceOrbEntity;
 import net.minecraft.network.PacketByteBuf;
@@ -7,11 +10,20 @@ import net.minecraft.network.listener.ClientPlayPacketListener;
 import net.minecraft.network.packet.Packet;
 
 public class OrbPacket implements Packet<ClientPlayPacketListener> {
+    public static final Identifier PACKET_ID = SkillZMain.identifierOf("orb");
     private final int entityId;
     private final double x;
     private final double y;
     private final double z;
     private final int experience;
+
+    public OrbPacket(PacketByteBuf buf) {
+        this.entityId = buf.readVarInt();
+        this.x = buf.readDouble();
+        this.y = buf.readDouble();
+        this.z = buf.readDouble();
+        this.experience = buf.readShort();
+    }
 
     public OrbPacket(LevelExperienceOrbEntity orb) {
         this.entityId = orb.getId();
@@ -19,14 +31,6 @@ public class OrbPacket implements Packet<ClientPlayPacketListener> {
         this.y = orb.getY();
         this.z = orb.getZ();
         this.experience = orb.getExperienceAmount();
-    }
-
-    private OrbPacket(PacketByteBuf buf) {
-        this.entityId = buf.readVarInt();
-        this.x = buf.readDouble();
-        this.y = buf.readDouble();
-        this.z = buf.readDouble();
-        this.experience = buf.readShort();
     }
 
     @Override
